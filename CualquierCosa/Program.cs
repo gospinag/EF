fa3f941785e485a0f7ba95ce384c62b14ec85e66
German Alfonso Ospina Gutiérrez <german.ospina@mvm.com.co>
2020-10-05 13:49:27 -0500

    first commit

---
+++ CualquierCosa/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CualquierCosa
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
